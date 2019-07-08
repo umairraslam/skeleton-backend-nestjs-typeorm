6efe2d40e25d58dd284635eaec66c855370b1728
Umair Aslam <umair.aslam@afiniti.com>
2019-07-08 16:21:26 +0500

    Added Swagger Document.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from './config/config.service';
@@ -10,6 +11,15 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const host = configService.get<string>('host');
   const port = configService.get<number>('port');
+
+  const options = new DocumentBuilder()
+    .setTitle('Skeleton App Backend')
+    .setDescription('The backend for the Skeleton App')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port, host, () => logger.log(`Backend listening at ${host}:${port}`));
 }
