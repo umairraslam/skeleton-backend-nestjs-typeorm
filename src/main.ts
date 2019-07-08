6dd218fa40e511c099f4b72aab15d3294bd535d9
Umair Aslam <umair.aslam@afiniti.com>
2019-07-08 15:23:21 +0500

    Addedd Nest Logger.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Bootstrap');
+  const port = 3000;
+
+  await app.listen(port, () => logger.log(`Backend listening at ${port}`));
 }
 bootstrap();
